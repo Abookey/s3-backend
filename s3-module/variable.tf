fddab66910bf10bf79a8cad37e2175c539abd947
Faisal <saidu_faisal@yahoo.com>
2023-10-01 21:35:41 -0500

    fix: Module update

--- s3-module/variable.tf
+++ s3-module/variable.tf
@@ -6,12 +6,11 @@ variable "env" {
 variable "region" {
   type    = string
   default = "us-west-2"
-
 }
 
 variable "versioning" {
   type = string
-  default = "Enable"
+  default = "Enabled"
 }
 
 variable "create_vpc" {
